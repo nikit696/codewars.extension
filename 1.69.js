88c522438f23e7e6c9b90f7c9559c8d6fadbcd9c
nikit696 <vqwerty2099@gmail.com>
2022-02-05 18:15:30 +0300

    new

---
+++ 1.69.js
@@ -0,0 +1,3 @@
+function move (position, roll) {
+  return position + roll * 2// return the new position
+}
---
+++ 1.73.js
@@ -0,0 +1,12 @@
+function multiTable(number) {
+    let string = ""
+    for (let i = 1; i <= 10; i++){
+      let result = i * number
+      if (i !== 10){
+        string += `${i} * ${number} = ${result}\n`
+  } else {
+    string += `${i} * ${number} = ${result}`
+  }
+      }
+    return string
+    }
