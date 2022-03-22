0d9954465513088b564077d1b9d271c2e65ce719
nikit696 <vqwerty2099@gmail.com>
2022-03-22 17:29:36 +0300

    new

---
+++ 3.85.js
@@ -0,0 +1,6 @@
+function greetDevelopers(list) {
+  for (let i = 0; i < list.length; i++){
+    list[i]["greeting"] = `Hi ${list[i].firstName}, what do you like the most about ${list[i].language}?` 
+    }
+    return list
+}
